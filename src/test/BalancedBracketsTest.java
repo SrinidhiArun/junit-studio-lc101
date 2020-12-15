2bdec3fe375171a9ff3e8fedce2531358e26287f
Srinidhi <srinidhi>
2020-12-14 18:59:29 -0600

    Unit tests for balanced brackets

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,23 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void testWithEmptyString(){
+      assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
 
+    @Test
+    public void testUnBalancedBracketsInput(){
+      assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testNotInOrderBracketsInput(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void testWithBalancedBracketsInput(){
+       assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
 }
